9257743b3034058c179cb6138299ba7c08a0f556
Nguyên <nguyenln2204@gmail.com>
2019-11-06 22:17:25 +0700

    bug: UI responsive bug fixed

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -8,14 +8,12 @@ import '../assets/css/Contact.css'
 function Contact () {
     return (
         <div id="contact">
-            <div class="parallax"></div>
             <h1 id='title'>Get in touch with us!</h1>
             <div id='contact-detail'>
                 <ContactColumn contactIcon={PhoneIcon} contactTitle='Phone' contactContent='0941639193'/>
                 <ContactColumn contactIcon={AddressIcon} contactTitle='Address' contactContent='227 Nguyen Van Cu, District 5, HCM city'/>
                 <ContactColumn contactIcon={EmailIcon} contactTitle='Email' contactContent='nguyenln2204@gmail.com'/>
             </div>
-            <div class="parallax"></div>
         </div>
     )
 }
